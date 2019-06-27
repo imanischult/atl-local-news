e0eaa1ee355112b401f8eea23b5d2e6abea9151b
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-06-27 00:24:16 -0400

    pulling necessary content using cheerio

---
+++ server.js
@@ -0,0 +1,66 @@
+// Dependencies
+const express = require("express");
+const mongojs = require("mongojs");
+
+// Require axios and cheerio to make scraping possible
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+// Initialize Express
+const app = express();
+
+// Databse configurations
+const dbURL = "article";
+const collections = ["atlArticles"];
+
+// Hook mongojs config to the db variable
+const db = mongojs(dbURL, collections);
+db.on("error", err => {
+  console.log("Database Error:" + err);
+});
+
+//Let's first get the scraped data using node and cheerio. We'll be scraping articles from AJC
+
+axios.get("https://patch.com/georgia/atlanta").then(response => {
+  const $ = cheerio.load(response.data);
+  //For each element with an li tag and a class of "tease"...
+  const titleArr = [];
+  $("section.px-3.px-sm-4.d-flex.flex-row").each(function(index, element) {
+    //find the following data (saved in a variable)
+    let title = $(element)
+      .find("h2")
+      .find("a")
+      .attr("title");
+
+    let descr = $(element)
+      .find("p")
+      .text()
+      .trim()
+      .split("By ");
+
+    let link = $(element)
+      .find("h2")
+      .find("a")
+      .attr("href");
+
+    let img = $(element)
+      .find("section.d-none.d-sm-block")
+      .find("p.m-0.text-xs.badge-spacing")
+      .find("span")
+      .text()
+      .split(",");
+
+    titleArr.push({
+      title: title,
+      description: descr[0],
+      link: link,
+      image: img
+    });
+  });
+  console.log(titleArr);
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
